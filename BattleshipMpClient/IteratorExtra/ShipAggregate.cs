598ebeb44fdc908d5d503448b43100335b06a232
vytcka <vytas.sa1@gmail.com>
2023-12-19 19:54:06 +0200

    Add aggregates to iterator

---
+++ BattleshipMpClient/IteratorExtra/ShipAggregate.cs
@@ -0,0 +1,21 @@
+﻿using BattleshipMpClient.Factory.Ship;
+using BattleshipMpClient.IteratorExtra;
+using System.Collections.Generic;
+
+namespace BattleshipMp.IteratorExtra
+{
+    public class ShipAggregate : IAggregate<IShip>
+    {
+        private readonly List<IShip> _ships;
+
+        public ShipAggregate(List<IShip> ships)
+        {
+            _ships = ships;
+        }
+
+        public IIterator<IShip> CreateIterator()
+        {
+            return new ShipIterator(_ships);
+        }
+    }
+}
--- BattleshipMpServer/IteratorExtra/ShipSizeIterator.cs
+++ BattleshipMpServer/IteratorExtra/ShipSizeIterator.cs
@@ -1,5 +1,4 @@
 ﻿using BattleshipMpClient.IteratorExtra;
-using System;
 using System.Collections.Generic;
 
 namespace BattleshipMp.IteratorExtra
@@ -8,18 +7,21 @@ namespace BattleshipMp.IteratorExtra
     {
         private readonly HashSet<ShipSize> _hashSet;
         private int _currentIndex;
-        private bool hasIterated;
 
         public ShipSizeIterator(HashSet<ShipSize> hashSet)
         {
             _hashSet = hashSet;
             _currentIndex = 0;
-            hasIterated = false;
         }
 
         public bool HasNext()
         {
-            return !hasIterated;
+            if (_currentIndex < _hashSet.Count)
+                return true;
+
+            _currentIndex = 0;
+
+            return false;
         }
 
         public ShipSize Next()
@@ -31,21 +33,10 @@ namespace BattleshipMp.IteratorExtra
                 var nextItem = array[_currentIndex];
                 _currentIndex++;
 
-                if (_currentIndex >= _hashSet.Count)
-                {
-                    _currentIndex = 0;
-                    hasIterated = true;
-                }
-
                 return nextItem;
             }
 
             return null;
-        }
-
-        public void ResetIteration()
-        {
-            hasIterated = false;
         }
     }
 }
